200d1c835358ae7a00f08c84c7d22f88bab00671
Tianliang Yao <93028929+Metaphysicist0@users.noreply.github.com>
2024-04-23 22:51:33 +0800

    Add files via upload

---
+++ densenet/predict_all.py
@@ -0,0 +1,227 @@
+import os
+import json
+import os
+
+import matplotlib
+import pandas as pd
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from tqdm import tqdm
+import torch
+import sys
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from MyDataSet import MyDataSet
+import os
+import json
+from itertools import cycle
+import matplotlib
+import pandas as pd
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_curve, auc
+from tqdm import tqdm
+import numpy as np
+import torch
+from sklearn.metrics import precision_score, recall_score
+from sklearn.preprocessing import label_binarize
+from scipy import interp
+
+import sys
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from MyDataSet import MyDataSet
+
+from test_split import read_split_data
+import matplotlib
+import pandas as pd
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from tqdm import tqdm
+import torch
+import sys
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from MyDataSet import MyDataSet
+from test_split import read_split_data
+import os
+import json
+
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from model import densenet201
+import os
+import json
+
+import matplotlib
+import pandas as pd
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from tqdm import tqdm
+import torch
+import sys
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from MyDataSet import MyDataSet
+
+from test_split import read_split_data
+test_data_path = r"G:\EC\ECDataset\test"
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+    test_images_path, test_images_label = read_split_data(test_data_path)
+    batch_size = 64
+    img_size = 224
+    data_transform = transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    # 实例化训练数据集
+    test_dataset = MyDataSet(images_path=test_images_path, images_class=test_images_label, transform=data_transform)
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               num_workers=nw,
+                                               collate_fn=test_dataset.collate_fn)
+
+
+    # create model
+
+    net = densenet201(num_classes=4).to(device)
+
+    # load model weights
+    model_weight_path = r"G:\EC\classification\ResNet\weight\model201-480.pth"  # 训练后权重的路径
+
+    net.load_state_dict(torch.load(model_weight_path, map_location=device))
+    net = net.cuda()
+    net.eval()
+    with torch.no_grad():
+        accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
+        sample_num = 0
+        preds = []
+        all_labels = []
+        probsList = []
+        pro = []
+        test_loader = tqdm(test_loader, file=sys.stdout)
+        for step, data in enumerate(test_loader):
+            images, labels = data
+            sample_num += images.shape[0]
+            pred = net(images.to(device))
+            _, y_hat = torch.max(pred, 1)
+            probs = pred[:, 1]
+            preds.extend(y_hat.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+            probsList.extend(probs.cpu().numpy())
+            pro.extend(pred.cpu().numpy())
+            pred_classes = torch.max(pred, dim=1)[1]
+            accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+            test_loader.desc = "acc: {:.3f}".format(accu_num.item() / sample_num)
+            print(accu_num.item() / sample_num)
+        coarse_confusion_matrix = confusion_matrix(preds, all_labels, normalize="true")
+        plot_cfm1(coarse_confusion_matrix, r".\confusion_matrix", 'resnet')
+        results = pd.DataFrame({'predictions': probsList, 'labels': all_labels})
+        results.to_csv('my_model.csv', index=False)
+
+        y_score = pro
+        y_label = all_labels
+
+        y_label = np.array(y_label)
+        y_score = np.array(y_score)
+        y_label = label_binarize(y_label, classes=[0, 1, 2, 3])
+
+        n_classes = 4
+
+        # 计算每一类的ROC
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_label[:, i], y_score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # micro（方法二）
+        fpr["micro"], tpr["micro"], _ = roc_curve(y_label.ravel(), y_score.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+        # macro（方法一）
+        # First aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+        # Then interpolate all ROC curves at this points
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(n_classes):
+            mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+        # Finally average it and compute AUC
+        mean_tpr /= n_classes
+        fpr["macro"] = all_fpr
+        tpr["macro"] = mean_tpr
+        roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+        # Plot all ROC curves
+        lw = 2
+        plt.figure(figsize=(8, 6))
+        plt.plot(fpr["micro"], tpr["micro"],
+                 label='micro-average ROC curve (AUC = {0:0.4f})'
+                       ''.format(roc_auc["micro"]),
+                 color='deeppink', linestyle=':', linewidth=4)
+
+        plt.plot(fpr["macro"], tpr["macro"],
+                 label='macro-average ROC curve (AUC = {0:0.4f})'
+                       ''.format(roc_auc["macro"]),
+                 color='navy', linestyle=':', linewidth=4)
+
+        colors = cycle(['aqua', 'darkorange', 'cornflowerblue', 'red'])
+
+        for i, color in zip(range(n_classes), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                     label='ROC curve of class {0} (AUC = {1:0.4f})'
+                           ''.format(i, roc_auc[i]))
+
+        plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate', fontsize=14)
+        plt.ylabel('True Positive Rate', fontsize=14)
+        plt.title('ROC curves of DenseNet169', fontsize=16)
+        plt.legend(loc="lower right", fontsize=12)
+        plt.show()
+
+        return accu_num.item() / sample_num
+
+
+def plot_cfm1(cf_matrix, save_path, experiment_name):
+    # Set the font family for Chinese characters (SimSun) and numbers (Euclid)
+    font = {'family': 'Times New Roman', 'weight': 'normal', 'size': 16}
+    matplotlib.rc('font', **font)
+
+    class_names = ['MMRd','NSMP','P53abn','POLEmut']
+
+
+    # cf_matrix = confusion_matrix(real, pred, normalize='true')
+
+    disp = ConfusionMatrixDisplay(confusion_matrix=cf_matrix, display_labels=class_names)
+
+    # d = {'vmin':0,'vmax':0.8}
+    disp.plot(cmap='Blues',  values_format='.2f')
+    plt.tight_layout()
+
+    # Set custom labels for the bottom and left of the confusion matrix plot
+    disp.ax_.set_xlabel('Predicted Image-based Molecular Class of EC', fontsize=14)
+    disp.ax_.set_ylabel('True Molecular Class of EC', fontsize=14)
+    disp.ax_.set_title('DenseNet Confusion Matrix (test)', fontsize=14)
+
+    plt.savefig(os.path.join(save_path))
+    plt.show()
+    plt.close()
+
+    specificity = cf_matrix.diagonal()/cf_matrix.sum(axis=1)
+    sensitivity = cf_matrix.diagonal()/cf_matrix.sum(axis=0)
+
+    print(f'Specificity: {specificity}')
+    print(f'Sensitivity: {sensitivity}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ densenet/train.py
@@ -0,0 +1,154 @@
+import os
+import math
+import argparse
+
+import torch
+import torch.optim as optim
+from matplotlib import pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+import torch.optim.lr_scheduler as lr_scheduler
+
+from model import densenet121, load_state_dict
+from my_dataset import MyDataSet
+from utils import read_split_data, train_one_epoch, evaluate
+
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+
+    print(args)
+    # print('Start Tensorboard with "tensorboard --logdir=runs", view at http://localhost:6006/')
+    tb_writer = SummaryWriter()
+    if os.path.exists("./weights") is False:
+        os.makedirs("./weights")
+
+    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(args.data_path)
+
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        "val": transforms.Compose([transforms.Resize(256),
+                                   transforms.CenterCrop(224),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+
+    # 实例化训练数据集
+    train_dataset = MyDataSet(images_path=train_images_path,
+                              images_class=train_images_label,
+                              transform=data_transform["train"])
+
+    # 实例化验证数据集
+    val_dataset = MyDataSet(images_path=val_images_path,
+                            images_class=val_images_label,
+                            transform=data_transform["val"])
+
+    batch_size = args.batch_size
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               num_workers=nw,
+                                               collate_fn=train_dataset.collate_fn)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=batch_size,
+                                             shuffle=False,
+                                             pin_memory=True,
+                                             num_workers=nw,
+                                             collate_fn=val_dataset.collate_fn)
+
+    # 如果存在预训练权重则载入
+    model = densenet121(num_classes=args.num_classes).to(device)
+    if args.weights != "":
+        if os.path.exists(args.weights):
+            load_state_dict(model, args.weights)
+        else:
+            raise FileNotFoundError("not found weights file: {}".format(args.weights))
+
+    # 是否冻结权重
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除最后的全连接层外，其他权重全部冻结
+            if "classifier" not in name:
+                para.requires_grad_(False)
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.SGD(pg, lr=args.lr, momentum=0.9, weight_decay=1E-4, nesterov=True)
+    # Scheduler https://arxiv.org/pdf/1812.01187.pdf
+    lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
+    train_losses = []
+    val_losses = []
+    val_accuracies = []
+    for epoch in range(args.epochs):
+        # train
+        mean_loss = train_one_epoch(model=model,
+                                    optimizer=optimizer,
+                                    data_loader=train_loader,
+                                    device=device,
+                                    epoch=epoch)
+        train_losses.append(mean_loss)
+
+        scheduler.step()
+
+        # validate
+        val_loss, val_acc = evaluate(model=model,
+                                     data_loader=val_loader,
+                                     device=device,
+                                     epoch=epoch)
+
+        val_losses.append(val_loss)
+        val_accuracies.append(val_acc)
+
+        # Save model checkpoint
+        torch.save(model.state_dict(), "./weights/model-{}.pth".format(epoch))
+
+        # Plotting
+    epochs = range(1, args.epochs + 1)
+
+    # Plot Loss
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, train_losses, label='Train Loss')
+    plt.plot(epochs, val_losses, label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    # Plot Accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, val_accuracies, label='Validation Accuracy')
+    plt.title('Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=4)
+    parser.add_argument('--epochs', type=int, default=200)
+    parser.add_argument('--batch-size', type=int, default=4)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--lrf', type=float, default=0.1)
+    #数据集所在根目录
+
+    parser.add_argument('--data-path', type=str,
+                        default=r"G:\EC\ECDataset")
+
+    # densenet121 官方权重下载地址
+    # https://download.pytorch.org/models/densenet121-a639ec97.pth
+    parser.add_argument('--weights', type=str, default=r"G:\EC\classification\weight\densenet121-a639ec97.pth",
+                        help='initial weights path')
+    parser.add_argument('--freeze-layers', type=bool, default=False)
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+
+    opt = parser.parse_args()
+
+    main(opt)
